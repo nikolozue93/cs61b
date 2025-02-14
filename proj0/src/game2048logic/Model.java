bd1d5153b594e1e6999b427a84c2c7d8a816b257
Nikolozu <nikaberidze27@gmail.com>
2025-02-14 16:59:20 +0400

    task1 proj0 completed

--- proj0/src/game2048logic/Model.java
+++ proj0/src/game2048logic/Model.java
@@ -86,6 +86,16 @@ public class Model {
      * */
     public boolean emptySpaceExists() {
         // TODO: Task 1. Fill in this function.
+        int size = board.size();
+
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                //Tile t = board.tile(i, j);
+                if(board.tile(i, j) == null){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
