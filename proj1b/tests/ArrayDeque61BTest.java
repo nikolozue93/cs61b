76b7b74ca1724244ebfc84ade6fe886b5d750fb8
Nikolozu <nikaberidze27@gmail.com>
2025-02-28 01:41:52 +0400

    Implemented basic structure, addFirst/addLast/isEmpty methods

--- proj1b/tests/ArrayDeque61BTest.java
+++ proj1b/tests/ArrayDeque61BTest.java
@@ -1,12 +1,6 @@
-import jh61b.utils.Reflection;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.Field;
-import java.util.List;
-
 import static com.google.common.truth.Truth.assertThat;
-import static com.google.common.truth.Truth.assertWithMessage;
 
 public class ArrayDeque61BTest {
 
@@ -19,5 +13,35 @@ public class ArrayDeque61BTest {
 //
 //         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
 //     }
+
+    @Test
+    /** This test performs interspersed addFirst and addLast calls. */
+    public void addFirstAndAddLastTest() {
+        Deque61B<Integer> lld1 = new ArrayDeque61B<>();
+
+         /* I've decided to add in comments the state after each call for the convenience of the
+            person reading this test. Some programmers might consider this excessively verbose. */
+        lld1.addLast(0);   // [0]
+        lld1.addLast(1);   // [0, 1]
+        lld1.addFirst(-1); // [-1, 0, 1]
+        lld1.addLast(2);   // [-1, 0, 1, 2]
+        lld1.addFirst(-2); // [-2, -1, 0, 1, 2]
+        lld1.addFirst(3);
+        lld1.addLast(4);
+        lld1.addLast(8);
+
+        Deque61B<Character> lld2 = new ArrayDeque61B<>();
+        lld2.addLast('a');
+        lld2.addLast('b');
+        lld2.addFirst('c');
+        lld2.addLast('d');
+        lld2.addLast('e');
+        lld2.addFirst('f');
+        lld2.addLast('g');
+        lld2.addFirst('h');
+
+
+        assertThat(lld1.toList()).containsExactly(-2, -1, 0, 1, 2).inOrder();
+    }
 
 }
