6201062536558e6fc66481e5f518069887f8d8ad
Nikolozu <nikaberidze27@gmail.com>
2025-04-25 16:03:54 +0400

    implemented constructor and find

--- lab05/src/UnionFind.java
+++ lab05/src/UnionFind.java
@@ -1,10 +1,14 @@
 public class UnionFind {
-    // TODO: Instance variables
+    private int[] union;
 
     /* Creates a UnionFind data structure holding N items. Initially, all
        items are in disjoint sets. */
     public UnionFind(int N) {
-        // TODO: YOUR CODE HERE
+        union = new int[N];
+
+        for(int i = 0; i < this.union.length; i++){
+            union[i] = -1;
+        }
     }
 
     /* Returns the size of the set V belongs to. */
@@ -29,9 +33,59 @@ public class UnionFind {
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. If invalid items are passed into this
        function, throw an IllegalArgumentException. */
+
+    /* we should search for elements with whatever the value our v holds
+    until we find sth less than 0.
+     */
     public int find(int v) {
-        // TODO: YOUR CODE HERE
-        return -1;
+        if(v > this.union.length){ // dunno if its right
+            throw new IllegalArgumentException();
+        }
+
+        if(union[v] < 0){
+            return v;
+        }
+
+//        int parent = this.union[v]; // getting parent node
+//        int root = -1;
+//
+//        if(v != 0 || v != this.union.length - 1){
+//            if(this.union[v - 1] < parent){
+//                for(int i = v; union[i] >= 0; i--){
+//                    root = i;
+//                }
+//                return root;
+//            }
+
+//            if(this.union[v - 1] > parent){
+//                for(int i = v; union[i] >= 0; i++){
+//                    root = i;
+//                }
+//                return root;
+//            }
+//        }
+
+        int parent = union[v];
+        int id = v; // element index
+
+        while(parent >= 0){
+            parent = union[id];
+            id--;
+        }
+
+        boolean flag = false;
+        if(parent < 0){
+            flag = true;
+        }
+
+        int i = id + 1;
+        while(i < v && flag){
+            union[i] = id;
+            i++;
+        }
+
+
+        return id;
     }
 
     /* Connects two items V1 and V2 together by connecting their respective
