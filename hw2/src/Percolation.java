0b9d39733cabaa0b36dd439d6ba35186465db94a
Nikolozu <nikaberidze27@gmail.com>
2025-05-04 00:45:19 +0400

    implemented constructor, numofopens, validate, xyTo1d and some basic outlines

--- hw2/src/Percolation.java
+++ hw2/src/Percolation.java
@@ -1,38 +1,67 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+import java.sql.Array;
+
 
 public class Percolation {
-    // TODO: Add any necessary instance variables.
+    int[][] A;
+    boolean[][] B;
+    int openSite;
+    int N;
+    WeightedQuickUnionUF wqUnion;
 
     public Percolation(int N) {
-        // TODO: Fill in this constructor.
+        // I think we need to add upper and lower lvls for bounds
+        // to have easy check on isFull or not
+        // maybe N + 1 and N + 1
+        this.N = N;
+        A = new int[N][N];
+        B = new boolean[N][N];  // to keep up with open sites
+        openSite = 0;
+        wqUnion = new WeightedQuickUnionUF(N * N);
     }
 
     public void open(int row, int col) {
-        // TODO: Fill in this method.
+        if(!isOpen(row, col)) {
+            validate(row, col);
+
+            B[row][col] = true;
+            openSite++;
+
+            // i think we have to check here up down left right sites
+            // if they are open and then make a decision with union
+        }
     }
 
     public boolean isOpen(int row, int col) {
-        // TODO: Fill in this method.
+        validate(row, col);
         return false;
     }
 
     public boolean isFull(int row, int col) {
-        // TODO: Fill in this method.
+        validate(row, col);
         return false;
     }
 
     public int numberOfOpenSites() {
-        // TODO: Fill in this method.
-        return 0;
+        return openSite;
     }
 
     public boolean percolates() {
-        // TODO: Fill in this method.
+        // if upper and lower bounds are connected (in union) return true
         return false;
     }
 
-    // TODO: Add any useful helper methods (we highly recommend this!).
-    // TODO: Remove all TODO comments before submitting.
+    public int xyTo1d(int r, int c){
+        validate(r, c);
+
+        return r * N + c; // number of row multiplied by the grid size N and added column number
+    }
+
+    public void validate(int r, int c){
+        if(0 > r || 0 > c || r >= N || c >= N){
+            throw new IllegalArgumentException("Index out of bounds");
+        }
+    }
 
 }
